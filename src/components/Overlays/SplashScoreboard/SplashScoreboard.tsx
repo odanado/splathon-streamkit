afc1a10609fca13144235eb61ece7277fdd40dd4
meguu <meguu08@gmail.com>
2023-04-06 18:40:37 +0900

    run lint

--- src/components/Overlays/SplashScoreboard/SplashScoreboard.tsx
+++ src/components/Overlays/SplashScoreboard/SplashScoreboard.tsx
@@ -1,10 +1,5 @@
-import type { Match } from "../../../schema/match";
 import classes from "./SplashScoreboard.module.css";
 
-type Props = {
-  match: Match;
-};
-
-export const SplashScoreboard = ({ match }: Props) => { 
+export const SplashScoreboard = () => { 
   return <div className={classes.background}></div>;
 };
